f94a724581497640c277f6d523df9015a8ae9db2
Leonhard Tissen <contact@warze.org>
2024-04-30 22:03:12 +0200

    Feature: Better kill animation

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,14 +1,8 @@
 extends Area2D
 
-@onready var timer = $Timer
 @onready var hurt_sound = $HurtSound
+@onready var player = %Player
 
 func _on_body_entered(body):
-	Engine.time_scale = 0.3
-	body.get_node('CollisionShape2D').queue_free()
-	timer.start()
-	hurt_sound.play()
+	body.kill()
 
-func _on_timer_timeout():
-	Engine.time_scale = 1.0
-	get_tree().reload_current_scene()
--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,32 @@ const JUMP_VELOCITY = -215.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var checkpoint = Vector2(0, 0)
+
+var is_dead = false
+
+func _ready():
+	# Set the checkpoint to the initial position.
+	checkpoint = position
+
+func _go_to_checkpoint():
+	# Teleport the player to the checkpoint.
+	position = checkpoint
+
+func kill():
+	print("Player died!")
+
+	is_dead = true
+	animated_sprite.play('death')
+	Engine.time_scale = 0.3
+
+	await get_tree().create_timer(0.4).timeout
+
+	# Reset the player.
+	is_dead = false
+	Engine.time_scale = 1.0
+	_go_to_checkpoint()
+
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var jump_sound = $JumpSound
 
@@ -13,6 +39,12 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+
+	# Handle death.
+	if is_dead:
+		velocity.x = 0
+		move_and_slide()
+		return
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
